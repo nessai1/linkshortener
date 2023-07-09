6b4e66de3d5b8e8e8d77fe8c0371f00652c8d76b
nessai1 <werowkin@gmail.com>
2023-07-09 19:26:18 +0200

    Визуальные правки в юниттестах

--- internal/shortener/decoder/encoder_test.go
+++ internal/shortener/decoder/encoder_test.go
@@ -9,48 +9,48 @@ import (
 func TestEncodeURL(t *testing.T) {
 	tests := []struct {
 		name         string
-		firstUrl     string
-		secondUrl    string
+		firstURL     string
+		secondURL    string
 		hasCollision bool
 	}{
 		{
 			name:         "Two different urls",
-			firstUrl:     "https://ya.ru",
-			secondUrl:    "https://vk.com",
+			firstURL:     "https://ya.ru",
+			secondURL:    "https://vk.com",
 			hasCollision: false,
 		},
 		{
 			name:         "Two equal urls",
-			firstUrl:     "https://ya.ru",
-			secondUrl:    "https://ya.ru",
+			firstURL:     "https://ya.ru",
+			secondURL:    "https://ya.ru",
 			hasCollision: true,
 		},
 		{
 			name:         "Two urls with little difference",
-			firstUrl:     "https://ya.ru",
-			secondUrl:    "https://ya.ru/",
+			firstURL:     "https://ya.ru",
+			secondURL:    "https://ya.ru/",
 			hasCollision: false,
 		},
 		{
 			name:         "Another two equal urls",
-			firstUrl:     "https://vkontakte.ru",
-			secondUrl:    "https://vkontakte.ru",
+			firstURL:     "https://vkontakte.ru",
+			secondURL:    "https://vkontakte.ru",
 			hasCollision: true,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			firstToken, err := EncodeURL(tt.firstUrl)
+			firstToken, err := EncodeURL(tt.firstURL)
 			if err != nil {
-				require.NoErrorf(t, err, "First encode of %s has error %s", tt.firstUrl, err.Error())
+				require.NoErrorf(t, err, "First encode of %s has error %s", tt.firstURL, err.Error())
 			}
 
-			secondToken, err := EncodeURL(tt.secondUrl)
+			secondToken, err := EncodeURL(tt.secondURL)
 			if err != nil {
-				require.NoErrorf(t, err, "Second encode of %s has error %s", tt.secondUrl, err.Error())
+				require.NoErrorf(t, err, "Second encode of %s has error %s", tt.secondURL, err.Error())
 			}
 
-			assert.Equal(t, tt.hasCollision, firstToken == secondToken, "Encode of %s and %s has collisions (%s)", tt.firstUrl, tt.secondUrl, secondToken)
+			assert.Equal(t, tt.hasCollision, firstToken == secondToken, "Encode of %s and %s has collisions (%s)", tt.firstURL, tt.secondURL, secondToken)
 		})
 	}
 }
