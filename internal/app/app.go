f0918df892cff19b17e00d339f7a23d469ad7554
nessai1 <werowkin@gmail.com>
2023-08-28 21:19:38 +0200

    Use buffered channel with signal.Notify

--- internal/app/app.go
+++ internal/app/app.go
@@ -27,7 +27,7 @@ func Run(handler ApplicationHandler, envType EnvType) {
 	fillRouter(router, handler.GetEndpoints(), "")
 	logger.Info(fmt.Sprintf("staring server on addr: %s", handler.GetAddr()))
 
-	c := make(chan os.Signal)
+	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	go func() {
 		<-c
