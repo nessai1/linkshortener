10309f2e3ade5a8e5ff9c31c9b6a535398d7ed24
nessai1 <werowkin@gmail.com>
2023-09-11 22:47:20 +0200

    Пересмотрел работу драйвера хранилища ссылок для поддержки работы вставки строк в sql драйвере

--- internal/shortener/linkstorage/inMemoryStorageDriver.go
+++ internal/shortener/linkstorage/inMemoryStorageDriver.go
@@ -1,14 +1,25 @@
 package linkstorage
 
-type InMemoryStorageDriver struct{}
+type InMemoryStorageDriver struct {
+	hl HashToLink
+}
 
-func (driver *InMemoryStorageDriver) Save(hl HashToLink) error {
+func (driver *InMemoryStorageDriver) Set(key string, val string) {
+	driver.hl[key] = val
+}
+
+func (driver *InMemoryStorageDriver) Get(key string) (string, bool) {
+	val, ok := driver.hl[key]
+	return val, ok
+}
+
+func (driver *InMemoryStorageDriver) Save() error {
 	return nil
 }
 
-func (driver *InMemoryStorageDriver) Load() (HashToLink, error) {
-	hl := make(HashToLink, 0)
-	return hl, nil
+func (driver *InMemoryStorageDriver) Load() error {
+	driver.hl = make(HashToLink, 0)
+	return nil
 }
 
 func (driver *InMemoryStorageDriver) Close() error {
--- internal/shortener/linkstorage/linkStorage.go
+++ internal/shortener/linkstorage/linkStorage.go
@@ -5,27 +5,29 @@ import "fmt"
 type HashToLink map[string]string
 
 type StorageDriver interface {
-	Save(hl HashToLink) error
-	Load() (HashToLink, error)
+	Set(key string, val string)
+	Get(key string) (string, bool)
+
+	Save() error
+	Load() error
 	Close() error
 }
 
 type Storage struct {
-	driver     StorageDriver
-	hashToLink HashToLink
+	driver StorageDriver
 }
 
 func (storage *Storage) Set(key string, val string) {
-	storage.hashToLink[key] = val
+	storage.driver.Set(key, val)
 }
 
 func (storage *Storage) Get(key string) (string, bool) {
-	val, ok := storage.hashToLink[key]
+	val, ok := storage.driver.Get(key)
 	return val, ok
 }
 
 func (storage *Storage) Save() error {
-	return storage.driver.Save(storage.hashToLink)
+	return storage.driver.Save()
 }
 
 func CreateStorage(driver StorageDriver) (*Storage, error) {
@@ -33,11 +35,10 @@ func CreateStorage(driver StorageDriver) (*Storage, error) {
 		driver: driver,
 	}
 
-	hl, err := driver.Load()
+	err := storage.driver.Load()
 	if err != nil {
 		return nil, fmt.Errorf("cannot load data from driver: %s", err.Error())
 	}
 
-	storage.hashToLink = hl
 	return &storage, nil
 }
