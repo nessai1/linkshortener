49f5fd1f70e2583695d9dc2c50e52e31284e6bd1
Daniil Verevkin <werowkin@gmail.com>
2023-12-25 21:46:28 +0200

    Покрыл тестами метод загрузки батчи адресов

--- internal/shortener/api_router_test.go
+++ internal/shortener/api_router_test.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"encoding/json"
 	"github.com/nessai1/linkshortener/internal/app"
+	"github.com/nessai1/linkshortener/internal/shortener/encoder"
 	"github.com/nessai1/linkshortener/internal/shortener/linkstorage"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -101,7 +102,7 @@ func TestApplication_apiHandleAddURL(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			r := httptest.NewRequest(http.MethodPost, testApp.GetAddr(), strings.NewReader(tt.input.body))
+			r := httptest.NewRequest(http.MethodPost, testApp.GetAddr()+"/api/shorten", strings.NewReader(tt.input.body))
 
 			if tt.input.userUUID != "" {
 				ctx := context.WithValue(r.Context(), app.ContextUserUUIDKey, app.UserUUID(tt.input.userUUID))
@@ -125,6 +126,109 @@ func TestApplication_apiHandleAddURL(t *testing.T) {
 				require.NoError(t, err)
 
 				assert.Equal(t, *tt.expectedLink, existingHashes[hash])
+			}
+		})
+	}
+}
+
+func TestApplication_apiHandleAddBatchURL(t *testing.T) {
+	ownerUUID := "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+
+	existingHashes := make(linkstorage.HashToLink, 2)
+	testApp := createTestApp("localhost:8080", TestTokenTail, existingHashes)
+
+	vkHash, _ := encoder.EncodeURL("https://vk.com")
+	yaHash, _ := encoder.EncodeURL("https://ya.ru")
+
+	tests := []struct {
+		name           string
+		input          InputDataJSON
+		expectedStatus int
+		expectedHashes map[string]string // correlation id -> link
+	}{
+		{
+			name: "Successful create: two links",
+			input: InputDataJSON{
+				body:     `[{"correlation_id": "a", "original_url": "https://vk.com"},{"correlation_id": "b", "original_url": "https://ya.ru"}]`,
+				userUUID: ownerUUID,
+			},
+			expectedStatus: http.StatusCreated,
+			expectedHashes: map[string]string{
+				"a": vkHash,
+				"b": yaHash,
+			},
+		},
+		{
+			name: "Successful create: one link",
+			input: InputDataJSON{
+				body:     `[{"correlation_id": "a", "original_url": "https://vk.com"}]`,
+				userUUID: ownerUUID,
+			},
+			expectedStatus: http.StatusCreated,
+			expectedHashes: map[string]string{
+				"a": vkHash,
+			},
+		},
+		{
+			name: "Unsuccessful create: not authorized",
+			input: InputDataJSON{
+				body:     `[{"correlation_id": "a", "original_url": "https://vk.com"}]`,
+				userUUID: "",
+			},
+			expectedStatus: http.StatusForbidden,
+			expectedHashes: nil,
+		},
+		{
+			name: "Unsuccessful create: incorrect format",
+			input: InputDataJSON{
+				body:     `[{"correlation_id": "a", "original_url": "https://vk.com"]`,
+				userUUID: ownerUUID,
+			},
+			expectedStatus: http.StatusBadRequest,
+			expectedHashes: nil,
+		},
+		{
+			name: "Unsuccessful create: empty body",
+			input: InputDataJSON{
+				body:     ``,
+				userUUID: ownerUUID,
+			},
+			expectedStatus: http.StatusBadRequest,
+			expectedHashes: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			request := httptest.NewRequest(http.MethodPost, "/api/shorten/batch", strings.NewReader(tt.input.body))
+			if tt.input.userUUID != "" {
+				ctx := context.WithValue(request.Context(), app.ContextUserUUIDKey, app.UserUUID(tt.input.userUUID))
+				request = request.WithContext(ctx)
+			}
+
+			writer := httptest.NewRecorder()
+			testApp.apiHandleAddBatchURL(writer, request)
+
+			assert.Equal(t, tt.expectedStatus, writer.Result().StatusCode)
+			if tt.expectedHashes != nil {
+				var buffer bytes.Buffer
+				n, err := buffer.ReadFrom(writer.Body)
+				require.NoError(t, err, "Test expected created list, got error while read")
+				require.NotEqual(t, 0, n, "Test expected created list, got empty body")
+
+				var result BatchResponse
+				err = json.Unmarshal(buffer.Bytes(), &result)
+				require.NoError(t, err, "Test expected created list, got error while unmarshal result body")
+
+				assert.Equal(t, len(tt.expectedHashes), len(result), "Len of expected hashes and actual must be equal")
+				for _, item := range result {
+					hash, err := fetchContentAfterTokenTail(item.ShortURL)
+					if err != nil {
+						assert.NoError(t, err, "Error while get hash from result link")
+					} else {
+						assert.Equal(t, tt.expectedHashes[item.CorrelationID], hash)
+					}
+				}
 			}
 		})
 	}
